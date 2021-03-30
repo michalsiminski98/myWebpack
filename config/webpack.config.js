dba84e97ad7846ab95a9b677d30aa3e3b4c28ecd
Michal Siminski <michal.siminski3@gmail.com>
2021-03-30 20:56:26 +0200

    end for live server

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,25 +1,38 @@
+// THIS WEBPACK CONFIG IS ONLY USED FOR DEV ONLY FOR LIVE VERSION: npm start
+
 const path = require('path');
 
 // plugins
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports ={
   mode: 'development',
   entry: './src/index.js',
-  output:{
-    filename: '[name]-[contenthash:6].js',
-    path: path.resolve(__dirname, "../", "devBuild"),
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: (__dirname, "src/index.html")
     }),
-    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/images", to: "assets/images" },
+      ],
+    }),
   ],
   devServer:{
     open:true,
-    contentBase: path.resolve(__dirname, '../assets'),
     port: 9000
   }
 }
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -3,18 +3,69 @@ const path = require('path');
 // plugins
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports ={
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output:{
-    filename: '[name]-[hash:6].js',
-    path: path.resolve(__dirname, "../", "devBuild"),
+    filename: '[name]-[contenthash:6].js',
+    path: path.resolve(__dirname, "../", "docs"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [{
+          loader: 'file-loader',
+          options:{
+            name: '[name].[ext]',
+            outputPath: 'images',
+          }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+            mozjpeg: {
+              quality: 70,
+              progressive: true
+            }
+          }
+          },
+        ],
+      }
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: (__dirname, "src/index.html")
     }),
+    new HtmlWebpackPlugin({
+      template: (__dirname, "src/about.html"),
+      filename:'about.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name]-[contenthash:6].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/images", to: "assets/images" },
+      ],
+    }),
     new CleanWebpackPlugin(),
   ],
+  devServer:{
+    open:true,
+    contentBase: path.resolve(__dirname, '../assets'),
+    port: 9000
+  }
 }
