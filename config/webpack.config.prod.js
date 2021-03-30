45e89762666dd246d2f4acb9cc1c2e8d4b50c008
Michal Siminski <michal.siminski3@gmail.com>
2021-03-30 22:32:17 +0200

    before smthing stupid

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -41,6 +41,16 @@ module.exports ={
             }
           }
           },
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
         ],
       }
     ],
