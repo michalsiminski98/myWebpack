3b5216556e34636a66b7b23b87441c0615af8d64
Michal Siminski <michal.siminski3@gmail.com>
2021-03-30 10:19:27 +0200

    add assets and live server

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path');
 
+// plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports ={
   mode: 'development',
   entry: './src/index.js',
   output:{
-    filename: 'index.js',
+    filename: '[name]-[contenthash:6].js',
     path: path.resolve(__dirname, "../", "devBuild"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: (__dirname, "src/index.html")
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer:{
+    open:true,
+    contentBase: path.resolve(__dirname, '../assets'),
+    port: 9000
   }
 }
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,10 +1,20 @@
 const path = require('path');
 
+// plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports ={
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output:{
-    filename: 'index.js',
-    path: path.resolve(__dirname, "../", "docs"),
-  }
+    filename: '[name]-[hash:6].js',
+    path: path.resolve(__dirname, "../", "devBuild"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: (__dirname, "src/index.html")
+    }),
+    new CleanWebpackPlugin(),
+  ],
 }
